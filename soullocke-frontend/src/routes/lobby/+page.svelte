aa3a0872ffcd7490fb4025a8b45b10e7744316ab
Peat <dev@hendrikrakemann.de>
2026-03-14 03:30:41 +0100

    integrate basic get lobby hook into lobby page as an example

--- soullocke-frontend/src/routes/lobby/+page.svelte
+++ soullocke-frontend/src/routes/lobby/+page.svelte
@@ -2,13 +2,21 @@
 	import Card from '../../components/Card.svelte';
 	import PrimaryButton from '../../components/buttons/PrimaryButton.svelte';
 	import TextInput from '../../components/TextInput.svelte';
+	import { createQuery } from '@tanstack/svelte-query';
+	import { getLobbyOptions } from '../../api/generated/@tanstack/svelte-query.gen';
+
+	let lobbyId = $state('');
+	const lobby = createQuery(() => ({
+			...getLobbyOptions({ path: { id: lobbyId } }),
+			enabled: lobbyId.length > 0
+	}));
 
 	let lobbyName = $state('Test Lobby');
-
 
 	const handleBtnClick = (): void => {
 		console.log(lobbyName);
 	};
+
 
 </script>
 
@@ -26,6 +34,25 @@
 			</header>
 			<section slot="content">
 				<TextInput label="Lobby Name" bind:value={lobbyName} />
+			</section>
+			<section slot="footer">
+				<PrimaryButton variant="filled" onClick={handleBtnClick}>Create Lobby</PrimaryButton>
+			</section>
+		</Card>
+	</div>
+	<div class="col-start-2">
+		<Card>
+			<header slot="header">
+				<h2 class="h4">Get Lobby</h2>
+			</header>
+			<section slot="content">
+				<TextInput label="Lobby Name" bind:value={lobbyId} />
+				{#if lobby.isSuccess}
+					{lobby.data.name}
+				{/if}
+				{#if lobby.isError}
+					<p class="text-red-500">Error fetching lobby</p>
+				{/if}
 			</section>
 			<section slot="footer">
 				<PrimaryButton variant="filled" onClick={handleBtnClick}>Create Lobby</PrimaryButton>
