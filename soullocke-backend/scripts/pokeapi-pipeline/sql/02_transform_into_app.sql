74ebb038036631487387dc8f03eeec38efe9c448
Peat <dev@hendrikrakemann.de>
2026-03-27 01:45:58 +0100

    refactor script to iterate over array with stage tables, their respective csv files and columns, to download them and copy them into the stage table.

--- soullocke-backend/scripts/pokeapi-pipeline/sql/02_transform_into_app.sql
+++ soullocke-backend/scripts/pokeapi-pipeline/sql/02_transform_into_app.sql
@@ -8,14 +8,14 @@ $$
     BEGIN
         SELECT count(*)
         INTO total_count
-        FROM poke_stage.version v
-                 JOIN poke_stage.version_group vg ON vg.id = v.version_group_id;
+        FROM poke_stage.versions v
+                 JOIN poke_stage.version_groups vg ON vg.id = v.version_group_id;
 
         WITH inserted AS (
             INSERT INTO game_editions (id, name, generation)
                 SELECT v.identifier, initcap(replace(v.identifier, '-', ' ')), vg.generation_id
-                FROM poke_stage.version v
-                         JOIN poke_stage.version_group vg ON vg.id = v.version_group_id
+                FROM poke_stage.versions v
+                         JOIN poke_stage.version_groups vg ON vg.id = v.version_group_id
                 ON CONFLICT DO NOTHING
                 RETURNING id)
         SELECT count(*)
